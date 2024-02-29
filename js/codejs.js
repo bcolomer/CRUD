038c9879dd694b78cc166df0728a1936716604cf
Bárbara Colomer <116679460+bcolomer@users.noreply.github.com>
2024-02-29 23:05:16 +0100

    modificado error de campos

--- js/codejs.js
+++ js/codejs.js
@@ -10,8 +10,8 @@ form.addEventListener('submit', function(event) {
     const isValid = validateForm();
 
     if (isValid) {
-       // alert('El formulario es válido. Puedes enviarlo.');
          form.submit();
+       // alert('El formulario es válido. Puedes enviarlo.');
     }
 });
 
@@ -20,9 +20,10 @@ function validateForm() {
 
     isValid = validateField('fname', 'fnameError', 'Nombre', /^[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]+$/) && isValid;
     isValid = validateField('lname', 'lnameError', 'Apellidos', /^[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]+$/) && isValid;
-  
-isValid = validateField('telefono', 'telefonoError', 'Telefono', /^[0-9-+\s]+$/) && isValid;
-    isValid = validateField('fechaNacimiento', 'fechaNacimientoError', 'Fecha de Nacimiento', /^\d{4}-\d{2}-\d{2}$/) && isValid;isValid = validateSelectField('country', 'countryError', 'Ciudad') && isValid;
+    isValid = validateField('correoElectronico', 'correoElectronicoError', 'Correo Electrónico', /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/) && isValid;
+    isValid = validateField('telefono', 'telefonoError', 'Telefono', /^[0-9-+\s]+$/) && isValid;
+    isValid = validateField('fechaNacimiento', 'fechaNacimientoError', 'Fecha de Nacimiento', /^\d{4}-\d{2}-\d{2}$/) && isValid;
+    isValid = validateSelectField('country', 'countryError', 'Ciudad') && isValid;
     return isValid;
 }
 
@@ -34,16 +35,19 @@ function validateField(fieldName, errorId, fieldLabel, regex) {
     if (fieldValue === '') {
         error.textContent = `El campo ${fieldLabel} es obligatorio.`;
         error.style.visibility = 'visible'; 
-        return false;
+        isValid = false;
     } else if (!regex.test(fieldValue)) {
         error.textContent = `Por favor, ingrese los datos correctos para el campo ${fieldLabel}.`;
         error.style.visibility = 'visible'; 
+        isValid = false;
     } else {
         error.textContent = '';
         error.style.visibility = 'hidden'; 
-        return true;
     }
+
+    return isValid;
 }
+
 function validateSelectField(fieldName, errorId, fieldLabel) {
     const field = document.getElementById(fieldName);
     const error = document.getElementById(errorId);
